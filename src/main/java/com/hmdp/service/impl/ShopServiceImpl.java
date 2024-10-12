e47d53923a89447ce0920dade37116220a62672e
Echo Chain <echochain.xjtu@gmail.com>
2024-10-12 17:07:05 +0800

    完成商户缓存功能开发

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -17,4 +24,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        // existed in Redis
+        String shopStr = redisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        if (shopStr != null) {
+            Shop shop = JSONObject.parseObject(shopStr, Shop.class);
+            return Result.ok(shop);
+        }
+
+        // NOT existed in Redis
+        // BUT existed in DB
+        Shop shop = getById(id);
+        if (shop != null) {
+            shopStr = JSON.toJSONString(shop);
+            redisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, shopStr);
+            return Result.ok(shop);
+        }
+
+        // NOT existed in DB
+        return Result.fail("ShopId NOT EXISTED in DB");
+    }
 }
