5b46259b22646e863aca649dbe71dd93b97e99fc
Echo Chain <echochain.xjtu@gmail.com>
2024-10-16 22:47:49 +0800

    增加测试类验证Redisson分布式锁的可重入性

---
+++ src/test/java/com/hmdp/RedissonTest.java
@@ -0,0 +1,55 @@
+package com.hmdp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.redisson.RedissonLock;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Cheng Yihao
+ * @version 1.0
+ * @date 2024/10/16 17:37
+ * @comment
+ */
+@SpringBootTest
+@Slf4j
+public class RedissonTest {
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Test
+    void method1() {
+        RLock lock = redissonClient.getLock("lock");
+        boolean isLock = lock.tryLock();
+        if (!isLock) {
+            log.error("获取锁失败, 1");
+            return;
+        }
+        try {
+            log.info("获取锁成功, 1");
+            method2();
+        } finally {
+            log.info("释放锁, 1");
+            lock.unlock();
+        }
+    }
+
+
+    void method2() {
+        RLock lock = redissonClient.getLock("lock");
+        boolean isLock = lock.tryLock();
+        if (!isLock) {
+            log.error("获取锁失败, 2");
+            return;
+        }
+        try {
+            log.info("获取锁成功, 2");
+        } finally {
+            log.info("释放锁, 2");
+            lock.unlock();
+        }
+    }
+}
